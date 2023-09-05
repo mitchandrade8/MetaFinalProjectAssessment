8f8d3d7d5b230d36749ee45991d58c48920d7290
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-09-04 18:36:33 -0600

    Initial Commit

---
+++ MetaFinalProjectAssessment/MetaFinalProjectAssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaFinalProjectAssessmentApp.swift
+//  MetaFinalProjectAssessment
+//
+//  Created by Mitch Andrade on 9/4/23.
+//
+
+import SwiftUI
+
+@main
+struct MetaFinalProjectAssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
